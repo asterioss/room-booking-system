ba4a657027ade00197cc1211c2f4485f626c9585
Asterios <steliospao1@gmail.com>
2024-10-12 13:50:05 +0300

    initial commit

---
+++ src/main/java/com/acme/room_booking_system/RoomBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.acme.room_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomBookingSystemApplication.class, args);
+	}
+
+}
